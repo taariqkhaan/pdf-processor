4870fd69accfb428595affb1af3ccb59362f95cb
Tariq Khan <taariq.khaan@gmail.com>
2025-02-09 22:58:01 -0500

    dependencies

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using PdfTextExtractor.ViewModels;
+
+namespace PdfTextExtractor.Views;
+
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+
